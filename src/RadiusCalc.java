9c14d9271cc79842d5df1bb4b32c5752610008f4
sserrano2024 <sserrano2024@cchsdons.com>
2024-01-26 10:57:19 -0800

    Completed Assignment. All bugs fixed with comments

--- src/RadiusCalc.java
+++ src/RadiusCalc.java
@@ -19,44 +19,44 @@ public class RadiusCalc {
         if (i == 1){
             circleCalc();
         }else if (i == 2){
-            sCalc();
+            sphereCalc();
         }else if(i == 3){
             coneCalc();
         }else if(i == 4){
-            columnCalc();
+            columnCalc(); //fixed the correct number corresponding with the correct formula
         }
     }
     //calculated area of circle
-    public static void circleCalc(){
+    public static void circleCalc(){ //reordered methods and renamed them to make more clear and easily read
         Scanner s = new Scanner(System.in); //declare scanner
         System.out.println("enter the radius of your circle");
         int r = s.nextInt();
         double result = Math.PI * Math.pow(r, 2);
-        System.out.println(result);
+        System.out.println(result);//print result instead of just r
     }
-    public static void sCalc(){
+    public static void sphereCalc(){
         Scanner s = new Scanner(System.in);//declare scanner
-        System.out.println("enter the radius of your circle");
+        System.out.println("enter the radius of your sphere");
         int r = s.nextInt();
-        double result = (4/3) * Math.PI * Math.pow(r, 3);
-        System.out.println(r);
+        double result = (4.0/3.0)*Math.PI * Math.pow(r, 3); //added decimal places to 4/3 in order for it to be a double
+        System.out.println(result);
     }
     public static void coneCalc(){
         Scanner s = new Scanner(System.in); //declare scanner
         System.out.println("enter the radius of your cone, then height of your cone");
         int r = s.nextInt();
-        int h = s.nextInt();
-        double result = Math.PI * Math.pow(r, 2) * h;
+        int h = s.nextInt(); //added h int for the height input
+        double result = Math.PI * Math.pow(r, 2) * h/3; //corrected formula, needed to divide h by 3
         System.out.println(result);
     }
 
-    public static void columnCalc(){
+    public static void columnCalc(){ //specified the Calc
         Scanner s = new Scanner(System.in); //declare scanner
-        System.out.println("enter the radius of your cone, then height of your cone");
+        System.out.println("enter the radius of your column, then height of your column"); //added "column" text not "cone"
         int r = s.nextInt();
         int h = s.nextInt();
         double result = Math.PI * Math.pow(r, 2) * h;
-        System.out.println(r);
+        System.out.println(result);
     }
 
 
